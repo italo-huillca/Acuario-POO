cff362dadb93a2755dda92d757949e54e298e5ea
Italo H <huillca.italo@gmail.com>
2025-04-01 14:05:25 -0500

    Añadiendo inicializadores

--- src/acuario/Acuario.kt
+++ src/acuario/Acuario.kt
@@ -7,4 +7,11 @@ class Acuario(var largo: Int = 100, var ancho: Int = 20, var alto: Int = 40) {
                 "Largo: $largo cm " +
                 "Alto: $alto cm ")
     }
+    init {
+        println("inicializando acuario")
+    }
+    init {
+        // 1 litro = 1000 cm^3
+        println("Volumen: ${ancho * largo * alto / 1000} l")
+    }
 }
