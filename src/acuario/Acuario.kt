080f376cfc56d9eb228f972343a6cbe5f6d5f414
Italo H <huillca.italo@gmail.com>
2025-04-01 13:46:30 -0500

    Añadiendo constructor y su modo de uso a la clase Acuario

--- src/acuario/Acuario.kt
+++ src/acuario/Acuario.kt
@@ -1,10 +1,7 @@
 package Acuario
 
 
-class Acuario {
-    var ancho: Int = 20
-    var alto: Int = 40
-    var largo: Int = 100
+class Acuario(var largo: Int = 100, var ancho: Int = 20, var alto: Int = 40) {
     fun imprimirTamano() {
         println("Ancho: $ancho cm " +
                 "Largo: $largo cm " +
--- src/acuario/main.kt
+++ src/acuario/main.kt
@@ -2,9 +2,19 @@ package acuario
 import Acuario.Acuario
 
 fun construirAcuario() {
-    val miAcuario = Acuario()
-    miAcuario.imprimirTamano()
+    val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+    // altura y largo por defecto
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+    // ancho por defecto
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+    // todo personalizado
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
 }
+
 
 fun main() {
     construirAcuario()
