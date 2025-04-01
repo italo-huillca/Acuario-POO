595b8dfe4c8e213150f75560768d977f8700671d
Italo H <huillca.italo@gmail.com>
2025-04-01 14:22:09 -0500

    Agregando un getter de propiedad

--- src/acuario/Acuario.kt
+++ src/acuario/Acuario.kt
@@ -1,23 +1,24 @@
 package Acuario
 
-
 class Acuario(var largo: Int = 100, var ancho: Int = 20, var alto: Int = 40) {
     fun imprimirTamano() {
         println("Ancho: $ancho cm " +
                 "Largo: $largo cm " +
-                "Alto: $alto cm ")
+                "Alto: $alto cm "
+        )
+        // 1 l = 1000 cm^3
+        println("Volumen: $volumen l")
     }
     init {
         println("inicializando acuario")
     }
-    init {
-        // 1 litro = 1000 cm^3
-        println("Volumen: ${ancho * largo * alto / 1000} l")
-    }
+
     constructor(numeroDePeces: Int) : this() {
         // 2,000 cm^3 por pez + espacio extra para que no se derrame
 
         val tanque = numeroDePeces * 2000 * 1.1
         alto = (tanque / (largo * ancho)).toInt()
     }
+    val volumen: Int
+        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
 }
--- src/acuario/main.kt
+++ src/acuario/main.kt
@@ -2,22 +2,9 @@ package acuario
 import Acuario.Acuario
 
 fun construirAcuario() {
-    val acuario1 = Acuario()
-    acuario1.imprimirTamano()
-    // altura y largo por defecto
-    val acuario2 = Acuario(ancho = 25)
-    acuario2.imprimirTamano()
-    // ancho por defecto
-    val acuario3 = Acuario(alto = 35, largo = 110)
-    acuario3.imprimirTamano()
-    // todo personalizado
-    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
-    acuario4.imprimirTamano()
     val acuario6 = Acuario(numeroDePeces = 29)
     acuario6.imprimirTamano()
-    println("Volumen: ${acuario6.ancho * acuario6.largo * acuario6.alto / 1000} l")
 }
-
 
 fun main() {
     construirAcuario()
